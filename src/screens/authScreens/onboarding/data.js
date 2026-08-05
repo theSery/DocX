00c1194e415bdd4b380698a2fd36a36c1a334a30
theSery <babajanyan.seyran@gmail.com>
2026-08-05 23:02:15 +0400

    new onboarding screen

--- src/screens/authScreens/onboarding/data.js
+++ src/screens/authScreens/onboarding/data.js
@@ -1,22 +1,28 @@
-export const onboardingData = [
+import scen1 from '../../../assets/images/scen1.webp';
+import scen2 from '../../../assets/images/scen2.webp';
+import scen3 from '../../../assets/images/scen3.webp';
 
+export const onboardingData = [
   {
     id: 1,
     text: 'Ընտրեք Ձեզ անհրաժեշտ ձևանմուշը',
     textColor: '#FFFFFF',
     backgroundColor: 'rgba(29, 78, 216, 0.35)',
+    image: scen1,
   },
   {
     id: 2,
     text: 'Լրացրեք պահանջվող տվյալները',
     textColor: '#FFFFFF',
     backgroundColor: 'rgba(96, 165, 250, 0.35)',
+    image: scen2,
   },
   {
     id: 3,
     text: 'Ուղարկեք հասցեատիրոջը հենց մեր համակարգից',
     textColor: '#FFFFFF',
     backgroundColor: 'rgba(96, 165, 250, 0.35)',
+    image: scen3,
   },
 ];
 
